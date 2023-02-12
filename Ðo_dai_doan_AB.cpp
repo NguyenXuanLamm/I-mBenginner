23b271ce3ede40484718e21cf46cc4d908c80603
NguyenXuanLamm <125185697+NguyenXuanLamm@users.noreply.github.com>
2023-02-12 16:45:34 +0700

    Add files via upload

---
+++ Ðo_dai_doan_AB.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+   int A(int xA, int yA),B(int xB,yB)
+   double dAB;
+   cout>>"Nhap do dai x va y cua A ";
+   cin<<xA<<yA;
+   cout>>"Nhap do dai x va y cua B ";
+   cin<<xB<<yB;
+   int main(){
+   	dAB=sqrt(pow(xA-xB,2)+pow(yA-yB,2));
+   	cout" Do dai doan AB la :"<<dAB;
+   }
